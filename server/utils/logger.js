89ce419899f493aac056473f19157235d7675f95
Avi Bhatnagar <91671078+Avibhatnagar10@users.noreply.github.com>
2025-08-03 00:43:43 +0530

    Add files via upload

---
+++ server/utils/logger.js
@@ -0,0 +1,8 @@
+// utils/logger.js
+const { createLogger, transports, format } = require('winston');
+
+module.exports = createLogger({
+  level: 'info',
+  format: format.combine(format.timestamp(), format.simple()),
+  transports: [new transports.Console()]
+});
